f69b7c2f867bfd919efad66e18f980797e91615a
Kellen Presta <kpresta04@gmail.com>
2020-08-15 06:53:39 -0500

    images added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,25 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require("path")
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        "~": path.join(__dirname, "src/"),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `card-images`,
+        path: path.join(__dirname, `src`, `images`, `cards`),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
-
-export default function Home() {
+import { graphql } from "gatsby"
+export const query = graphql`
+  query {
+    cardImages: allFile(filter: { sourceInstanceName: { eq: "card-images" } }) {
+      edges {
+        node {
+          name
+          childImageSharp {
+            fluid(maxWidth: 100, maxHeight: 150, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default function Home({ data }) {
+  console.log(data.cardImages)
   return <div>Hello world!</div>
 }
